89b75c022dcde45df567362a165c1dc458cd51e4
Suad Suljovic <suad.suljovic92@gmail.com>
2023-12-25 21:04:07 +0100

    clean up and adding router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,33 @@
-import "./App.css";
-import { Button } from "antd";
 import { useSelector } from "react-redux";
+
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import ArticlePage from "./pages/ArticlePage";
+import GroupsPage from "./pages/GroupsPage";
+import MyLayout from "./components/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <MyLayout>
+        <Outlet />
+      </MyLayout>
+    ),
+    children: [
+      { index: true, element: <LandingPage /> },
+      { path: "article/:id", element: <ArticlePage /> },
+      { path: "groups", element: <GroupsPage /> },
+    ],
+  },
+]);
 
 function App() {
   const blogsState = useSelector((state) => state.blogs);
 
-  console.log(blogsState);
   return (
     <>
-      <Button>Text</Button>
+      <RouterProvider router={router} />
     </>
   );
 }
