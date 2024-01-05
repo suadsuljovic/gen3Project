b0cf1d5839727157fc5724e0c5c58409fee611e3
Suad Suljovic <suad.suljovic92@gmail.com>
2024-01-05 20:44:45 +0100

    update landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,32 +1,12 @@
-import { Button, Typography, theme } from "antd";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { changeLoading, fetchBlogs } from "../slices";
-const { Title, Text } = Typography;
-
-const { useToken } = theme;
+import CTA from "../components/CTA/CTA";
+import Hero from "../components/Hero/Hero";
 
 function LandingPage() {
-  const token = useToken();
-  const blogsState = useSelector((state) => state.blogs);
-  const dispatch = useDispatch();
-
-  console.log(blogsState);
-
-  useEffect(() => {
-    dispatch(changeLoading());
-    dispatch(fetchBlogs());
-  }, []);
-
   return (
-    <div>
-      Landing Page
-      <Button type="text">Text</Button>
-      <Typography>
-        <Title>ovo je title</Title>
-        <Text>Ovo je text</Text>
-      </Typography>
-    </div>
+    <>
+      <Hero />
+      <CTA />
+    </>
   );
 }
 
