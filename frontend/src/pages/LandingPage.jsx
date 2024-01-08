636ae4eeb1390328f08a88a3e1383194be74e82b
Suad Suljovic <suad.suljovic92@gmail.com>
2024-01-08 20:54:09 +0100

    add category groups to landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,13 +1,29 @@
+import { useDispatch, useSelector } from "react-redux";
 import CTA from "../components/CTA/CTA";
 import Hero from "../components/Hero/Hero";
-import Popular from "../components/Popular/Popular";
+import LandingCategoryGroup from "../components/Popular/LandingCategoryGroup";
+import { useEffect } from "react";
+import { fetchNatureBlogs, fetchNewsBlogs, fetchPopularBlogs } from "../slices";
 
 function LandingPage() {
+  const popularBlogs = useSelector((state) => state.blogs.popularBlogs);
+  const natureBlogs = useSelector((state) => state.blogs.natureBlogs);
+  const newsBlogs = useSelector((state) => state.blogs.newsBlogs);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPopularBlogs());
+    dispatch(fetchNatureBlogs());
+    dispatch(fetchNewsBlogs());
+  }, []);
+
   return (
     <>
       <Hero />
       <CTA />
-      <Popular />
+      <LandingCategoryGroup data={popularBlogs?.data} title="Popular blogs" />
+      <LandingCategoryGroup data={natureBlogs?.data} title="Nature blogs" />
+      <LandingCategoryGroup data={newsBlogs?.data} title="News" />
     </>
   );
 }
