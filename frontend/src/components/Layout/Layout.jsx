32d02721a5cee42bcf8eeafa940ce8da0a93e89e
Suad Suljovic <suad.suljovic92@gmail.com>
2024-01-12 20:35:21 +0100

    add search input

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,7 +1,11 @@
 import { Layout, Menu } from "antd";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import styles from "./layout.module.css";
 import { paths } from "../../utils";
+import { Input } from "antd";
+// import { useDispatch } from "react-redux";
+// import { searchArticle } from "../../slices/search";
+const { Search } = Input;
 
 const { Header, Content, Footer } = Layout;
 const items = [
@@ -13,6 +17,8 @@ const MyLayout = (props) => {
   const { children } = props;
   const navigate = useNavigate();
   const location = useLocation();
+  const { q } = useParams();
+  // const dispatch = useDispatch();
 
   const isLandingPage = location.pathname === paths.landing;
   const isGroupsPage = location.pathname === paths.group;
@@ -34,6 +40,18 @@ const MyLayout = (props) => {
             if (item.key === "2") {
               navigate(paths.group);
             }
+          }}
+        />
+        <Search
+          placeholder="input search text"
+          onSearch={(value) => {
+            // navigate("/search/" + value);
+            navigate(paths.search.replace(":q", value));
+            // dispatch(searchArticle(value));
+          }}
+          defaultValue={q}
+          style={{
+            width: 200,
           }}
         />
       </Header>
